d97217d45f5bbe96a9664ded7cf704cdbb968620
Allan Larangeiras <alarangeiras@gmail.com>
2024-08-04 17:02:07 -0300

    feat: change main function

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import express from 'express';
 import { initDB } from './infra/config/db';
 import { EnvironmentVariables } from './infra/config/env';
 import { initRoutes } from './infra/config/routes';
-(async () => {
+
+async function main() {
 	await initDB();
 	const app = express();
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	await initRoutes(app);
 	app.listen(EnvironmentVariables.PORT, () => {
-		console.log(`Server started at ${EnvironmentVariables.PORT}`);
+		console.log(
+			`Server started at http://127.0.0.1:${EnvironmentVariables.PORT}`,
+		);
 	});
-})();
+}
+
+main();
